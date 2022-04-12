76f0bb1e463a9681a493c32fbd755c982822eec0
mariabrinzila <58850569+mariabrinzila@users.noreply.github.com>
2022-04-12 17:20:21 +0300

    ADD design photos & flutter initial project

---
+++ flutter_proiect/lib/main.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const NotepadApp());
+}
+
+// Root of the application
+class NotepadApp extends StatelessWidget {
+  const NotepadApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'APad',
+      theme: ThemeData(
+        androidOverscrollIndicator: AndroidOverscrollIndicator.stretch,
+        primarySwatch: Colors.teal,
+      ),
+      home: const TodoList(),
+    );
+  }
+}
+
+class TodoList extends StatefulWidget {
+  const TodoList({Key? key}) : super(key: key);
+
+  @override
+  _TodoListState createState() => _TodoListState();
+}
+
+class _TodoListState extends State<TodoList> {
+  static const _appTitle = 'Todo List';
+  final todos = <String>[];
+  final controller = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: _appTitle,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(_appTitle),
+        ),
+        body: Column(
+          children: [
+            TextField(
+              controller: controller,
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: todos.length,
+                itemBuilder: (BuildContext context, int index) {
+                  final todo = todos[index];
+
+                  return Dismissible(
+                    key: Key('$todo$index'),
+                    onDismissed: (direction) => todos.removeAt(index),
+                    child: ListTile(title: Text(todo)),
+                    background: Container(color: Colors.red),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            setState(() {
+              todos.add(controller.text);
+              controller.clear();
+            });
+          },
+          child: const Icon(Icons.add),
+        ),
+      ),
+    );
+  }
+}
