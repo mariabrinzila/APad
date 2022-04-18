299627d0e9bf4600252916a3594beee8b46ccb1a
mariabrinzila <58850569+mariabrinzila@users.noreply.github.com>
2022-04-18 20:50:00 +0300

    ADD note model

---
+++ Flutter_project/model/note.dart
@@ -0,0 +1,16 @@
+class Note {
+  int id = 0;
+  String title;
+  String category;
+  String content;
+
+  Note(this.title, this.category, this.content);
+
+  void setId(int id) {
+    this.id = id;
+  }
+
+  // void edit(String content)
+
+  // void delete()
+}
